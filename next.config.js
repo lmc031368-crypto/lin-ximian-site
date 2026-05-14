a94de825b4fd9baa441dd6ef4891012274d02bce
cm l <lmc031368@gmail.com>
2026-05-14 16:49:18 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // 帮助搜索引擎识别你的新公司名
+  env: {
+    COMPANY_NAME: 'Lin Ximian Biotechnology Co., Ltd.',
+  },
+  images: {
+    domains: ['images.unsplash.com'], // 确保图片能正常显示
+  },
 }
+
 module.exports = nextConfig
